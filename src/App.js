b8043b9d14c1c452444ebfbdaf915bd4984b8be3
LaurensiusNath <nathan.surya0332@gmail.com>
2024-12-18 10:30:04 +0700

    deploy

--- src/App.js
+++ src/App.js
@@ -1,53 +1,6 @@
 import { useEffect, useState } from "react";
 import StarRating from "./StarRating";
 
-const tempMovieData = [
-  {
-    imdbID: "tt15398776",
-    Title: "Oppenheimer",
-    Year: "2013",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMDBmYTZjNjUtN2M1MS00MTQ2LTk2ODgtNzc2M2QyZGE5NTVjXkEyXkFqcGdeQXVyNzAwMjU2MTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt1517268",
-    Title: "Barbie",
-    Year: "2023",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNjU3N2QxNzYtMjk1NC00MTc4LTk1NTQtMmUxNTljM2I0NDA5XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt8589698",
-    Title: "Teenage Mutant Ninja Turtles: Mutant Mayhem",
-    Year: "2023",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYzE4MTllZTktMTIyZS00Yzg1LTg1YzAtMWQwZTZkNjNkODNjXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_SX300.jpg",
-  },
-];
-
-const tempWatchedData = [
-  {
-    imdbID: "tt15398776",
-    Title: "Oppenheimer",
-    Year: "2013",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMDBmYTZjNjUtN2M1MS00MTQ2LTk2ODgtNzc2M2QyZGE5NTVjXkEyXkFqcGdeQXVyNzAwMjU2MTY@._V1_SX300.jpg",
-    runtime: 180,
-    imdbRating: 8.6,
-    userRating: 10,
-  },
-  {
-    imdbID: "tt1517268",
-    Title: "Barbie",
-    Year: "2023",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNjU3N2QxNzYtMjk1NC00MTc4LTk1NTQtMmUxNTljM2I0NDA5XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg",
-    runtime: 114,
-    imdbRating: 7.2,
-    userRating: 8,
-  },
-];
-
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
@@ -166,7 +119,7 @@ function MovieDetail({ selectedId, onCloseMovie, onAddWatched, watched }) {
     async function getMovieDetail() {
       setIsLoading(true);
       const response = await fetch(
-        `http://www.omdbapi.com/?apikey=${API_KEY}&i=${selectedId}`
+        `https://www.omdbapi.com/?apikey=${API_KEY}&i=${selectedId}`
       );
       const data = await response.json();
       setMovie(data);
@@ -368,7 +321,7 @@ export default function App() {
         setisLoading(true);
         setIsError("");
         const response = await fetch(
-          `http://www.omdbapi.com/?s=${query}&apikey=${API_KEY}`,
+          `https://www.omdbapi.com/?s=${query}&apikey=${API_KEY}`,
           { signal: controller.signal }
         );
 
